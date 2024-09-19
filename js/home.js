8c5d96c1ee4b8b3f8e550f79c6224abe2180a6fc
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-19 09:48:21 +0600

    log out button

---
+++ js/home.js
@@ -0,0 +1,10 @@
+document.getElementById('log-out').addEventListener('click',function(){
+    window.location.href ='/index.html'
+})
+
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log(6656565656);
+    
+})
