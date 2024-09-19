6f1aa053d7ff53ff12d13e4567fa232f4e35ca23
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-19 07:19:52 +0600

    window location

--- js/login.js
+++ js/login.js
@@ -1,17 +1,18 @@
 // console.log(5454)
-document.getElementById('button-login').addEventListener('click',function(event){
+document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
 
-    const phoneNumber =document.getElementById('phone-Number').value;
-    const pinNumber =document.getElementById('pin-number').value;
-   if(phoneNumber ==='5' && pinNumber ==='1111'){
-    console.log("Enter your login in");
-    
-   }
-   else{
-    console.log('enter your number or pin');
-    
-   }
+    const phoneNumber = document.getElementById('phone-Number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    if (phoneNumber === '5' && pinNumber === '1111') {
+        console.log("Enter your login in");
+        window.location.href ='/home.html'
 
-    
+    }
+    else {
+        console.log('enter your number or pin');
+
+    }
+
+
 })
